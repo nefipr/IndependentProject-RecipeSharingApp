26dd720f5fee5825c07429de2b519e918033473b
Nefi Perez <nefipr@gmail.com>
2023-10-14 20:27:11 +0200

    quick fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('./swagger'))
+router.use('*/*', require('./swagger'))
 router.use('/recipes', require('./recipes'))
 
 module.exports = router;
