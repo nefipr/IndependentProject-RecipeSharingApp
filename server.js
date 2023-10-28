25ac6e6f06772d914c165f4649a579132478c44c
Nefi Perez <nefipr@gmail.com>
2023-10-29 01:31:36 +0200

    Include frontend Sign In Page

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const routes = require('./routes')
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 const app = express();
  
 app.get('/', (req, res) => {
-  res.send("Hello, here you will find the best Mexican Food, move to /recipes");
+  res.sendFile(path.join(__dirname, '/static/index.html'));
+  // res.send("Hello, here you will find the best Mexican Food, move to /recipes");
 });
 
 app
